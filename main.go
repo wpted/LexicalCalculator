1b89433160f390a68a1f5de6fa50961aed671e93
wpted <silverhandsome@gmail.com>
2024-01-04 10:08:23 +0800

    Refactor: Change if/else cause to switch/case in main().

--- main.go
+++ main.go
@@ -11,25 +11,25 @@ const (
 )
 
 func main() {
-    // We want a calculator that reads scripts like `calc '1 + 1'`, `calc '2 * 3 + 4'` or `end`.
+    // We want a calculator that reads scripts like `calc "1 + 1"`, `calc "2 * 3 + 4"` or `quit`.
     // Operators: +, -, *, /, **, (), [], {}.
+    // Should have prefix calculation like `calc "-5 + 4"`
     // Should handle error like zero-divisions.
     // TODO 0: Create CLI environment(repl), takes commands; ['calc', 'end'].
     fmt.Println("Start calculator.")
     scanner := bufio.NewScanner(os.Stdin)
     for {
-        scanned := scanner.Scan()
-        if !scanned {
-            return
-        }
+        scanner.Scan()
         cmd := scanner.Text()
-        if cmd == QUIT {
-            break
-        } else {
+
+        switch cmd {
+        case QUIT:
+            return
+        default:
             fmt.Println(cmd)
         }
-
     }
 
     // TODO 1: Read expressions +, -, *, /.
+    // Handle four simple expressions.
 }
