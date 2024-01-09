2a63df68d427ff38e1172aaf2e36f58d12468a4f
wpted <silverhandsome@gmail.com>
2024-01-09 08:13:43 +0800

    Test: Add test cases in TestEvaluate().

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -3,6 +3,11 @@ package ast
 import "testing"
 
 func TestEvaluate(t *testing.T) {
+    leaf1 := New(6, true, "", false, nil, nil)
+    leaf2 := New(3, true, "", false, nil, nil)
+    leaf3 := New(5, true, "", false, nil, nil)
+    leaf4 := New(0, false, "+", true, leaf1, leaf2)
+    leaf5 := New(0, false, "*", true, leaf1, leaf2)
     testCases := []struct {
         root   *Node
         left   *Node
@@ -10,10 +15,74 @@ func TestEvaluate(t *testing.T) {
         result float32
     }{
         {
-            root:   New(5, true, "", false, nil, nil),
+            // nil node
+            root:   nil,
             left:   nil,
             right:  nil,
-            result: 5,
+            result: 0,
+        },
+        {
+            // 6
+            root:   leaf1,
+            left:   nil,
+            right:  nil,
+            result: 6,
+        },
+        {
+            // -6
+            root:   New(0, false, "-", true, nil, leaf1),
+            left:   nil,
+            right:  leaf1,
+            result: -6,
+        },
+        {
+            // 6 + 3
+            root:   New(0, false, "+", true, leaf1, leaf2),
+            left:   leaf1,
+            right:  leaf2,
+            result: 9,
+        },
+        {
+            // 6 - 3
+            root:   New(0, false, "-", true, leaf1, leaf2),
+            left:   leaf1,
+            right:  leaf2,
+            result: 3,
+        },
+        {
+            // 6 * 3
+            root:   New(0, false, "*", true, leaf1, leaf2),
+            left:   leaf1,
+            right:  leaf2,
+            result: 18,
+        },
+        {
+            // 6 / 3
+            root:   New(0, false, "/", true, leaf1, leaf2),
+            left:   leaf1,
+            right:  leaf2,
+            result: 2,
+        },
+        {
+            // 6 / 3
+            root:   New(0, false, "/", true, leaf1, leaf2),
+            left:   leaf1,
+            right:  leaf2,
+            result: 2,
+        },
+        {
+            // 5 + 6 + 3
+            root:   New(0, false, "+", true, leaf3, leaf4),
+            left:   leaf3,
+            right:  leaf4,
+            result: 14,
+        },
+        {
+            // 5 + 6 * 3
+            root:   New(0, false, "+", true, leaf3, leaf5),
+            left:   leaf3,
+            right:  leaf5,
+            result: 23,
         },
     }
 
