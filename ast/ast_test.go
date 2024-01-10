605c49161d8749237113bcdffa870f69ee0885f3
wpted <silverhandsome@gmail.com>
2024-01-10 10:51:48 +0800

    Test: Create TestNode_String().

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -1,13 +1,95 @@
 package ast
 
-import "testing"
+import (
+    "testing"
+)
+
+func TestNode_String(t *testing.T) {
+    testCases := []struct {
+        node *Node
+        str  string
+    }{
+        {
+            node: New(nil, 1, true, "", false, nil, nil),
+            str:  "1",
+        },
+        {
+            node: New(nil, 0, false, "+", true,
+                nil,
+                New(nil, 2, true, "", false, nil, nil),
+            ),
+            str: "(+ 0 2)",
+        },
+        {
+            node: New(nil, 0, false, "+", true,
+                New(nil, 3, true, "", false, nil, nil),
+                New(nil, 2, true, "", false, nil, nil),
+            ),
+            str: "(+ 3 2)",
+        },
+        {
+            node: New(nil, 0, false, "-", true,
+                New(nil, 3, true, "", false, nil, nil),
+                New(nil, 2, true, "", false, nil, nil),
+            ),
+            str: "(- 3 2)",
+        },
+        {
+            node: New(nil, 0, false, "*", true,
+                New(nil, 3, true, "", false, nil, nil),
+                New(nil, 2, true, "", false, nil, nil),
+            ),
+            str: "(* 3 2)",
+        },
+        {
+            node: New(nil, 0, false, "/", true,
+                New(nil, 3, true, "", false, nil, nil),
+                New(nil, 2, true, "", false, nil, nil),
+            ),
+            str: "(/ 3 2)",
+        },
+        {
+            node: New(nil, 0, false, "+", true,
+                New(nil, 5, true, "", false, nil, nil),
+                New(nil, 2, false, "*", true,
+                    New(nil, 2, true, "", false, nil, nil),
+                    New(nil, 3, true, "", false, nil, nil),
+                ),
+            ),
+            str: "(+ 5 (* 2 3))",
+        },
+        {
+            node: New(nil, 0, false, "-", true,
+                New(nil, 0, false, "+", true,
+                    New(nil, 1, true, "", false, nil, nil),
+                    New(nil, 0, false, "*", true,
+                        New(nil, 2, true, "", false, nil, nil),
+                        New(nil, 5, true, "", false, nil, nil),
+                    ),
+                ),
+                New(nil, 0, false, "*", true,
+                    New(nil, 3, true, "", false, nil, nil),
+                    New(nil, 2, true, "", false, nil, nil),
+                ),
+            ),
+            str: "(- (+ 1 (* 2 5)) (* 3 2))",
+        },
+    }
+
+    for _, tc := range testCases {
+        sExpression := tc.node.String()
+        if sExpression != tc.str {
+            t.Errorf("Error transforming code into S-expression: expected %s, got %s.\n", tc.str, sExpression)
+        }
+    }
+}
 
 func TestEvaluate(t *testing.T) {
-    leaf1 := New(6, true, "", false, nil, nil)
-    leaf2 := New(3, true, "", false, nil, nil)
-    leaf3 := New(5, true, "", false, nil, nil)
-    leaf4 := New(0, false, "+", true, leaf1, leaf2)
-    leaf5 := New(0, false, "*", true, leaf1, leaf2)
+    leaf1 := New(nil, 6, true, "", false, nil, nil)
+    leaf2 := New(nil, 3, true, "", false, nil, nil)
+    leaf3 := New(nil, 5, true, "", false, nil, nil)
+    leaf4 := New(nil, 0, false, "+", true, leaf1, leaf2)
+    leaf5 := New(nil, 0, false, "*", true, leaf1, leaf2)
     testCases := []struct {
         root   *Node
         left   *Node
@@ -30,56 +112,56 @@ func TestEvaluate(t *testing.T) {
         },
         {
             // -6
-            root:   New(0, false, "-", true, nil, leaf1),
+            root:   New(nil, 0, false, "-", true, nil, leaf1),
             left:   nil,
             right:  leaf1,
             result: -6,
         },
         {
             // 6 + 3
-            root:   New(0, false, "+", true, leaf1, leaf2),
+            root:   New(nil, 0, false, "+", true, leaf1, leaf2),
             left:   leaf1,
             right:  leaf2,
             result: 9,
         },
         {
             // 6 - 3
-            root:   New(0, false, "-", true, leaf1, leaf2),
+            root:   New(nil, 0, false, "-", true, leaf1, leaf2),
             left:   leaf1,
             right:  leaf2,
             result: 3,
         },
         {
             // 6 * 3
-            root:   New(0, false, "*", true, leaf1, leaf2),
+            root:   New(nil, 0, false, "*", true, leaf1, leaf2),
             left:   leaf1,
             right:  leaf2,
             result: 18,
         },
         {
             // 6 / 3
-            root:   New(0, false, "/", true, leaf1, leaf2),
+            root:   New(nil, 0, false, "/", true, leaf1, leaf2),
             left:   leaf1,
             right:  leaf2,
             result: 2,
         },
         {
             // 6 / 3
-            root:   New(0, false, "/", true, leaf1, leaf2),
+            root:   New(nil, 0, false, "/", true, leaf1, leaf2),
             left:   leaf1,
             right:  leaf2,
             result: 2,
         },
         {
             // 5 + 6 + 3
-            root:   New(0, false, "+", true, leaf3, leaf4),
+            root:   New(nil, 0, false, "+", true, leaf3, leaf4),
             left:   leaf3,
             right:  leaf4,
             result: 14,
         },
         {
             // 5 + 6 * 3
-            root:   New(0, false, "+", true, leaf3, leaf5),
+            root:   New(nil, 0, false, "+", true, leaf3, leaf5),
             left:   leaf3,
             right:  leaf5,
             result: 23,
