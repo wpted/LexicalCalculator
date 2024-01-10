364260f12d3f4fdb7b08e60392f0b46df9989f64
wpted <silverhandsome@gmail.com>
2024-01-10 17:03:24 +0800

    Feat: Add lexer and parser to the repl. Evaluate prompt with the parser.

--- main.go
+++ main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+    "LexicalCalculator/lexer"
+    "LexicalCalculator/parser"
     "bufio"
     "fmt"
     "os"
 )
 
 const (
+    REPL = ">> "
     QUIT = "quit"
 )
 
 func main() {
+    // Initialize lexer and parser.
+    l := lexer.New()
+    p := parser.New(l)
+
     // We want a calculator that reads scripts like `calc "1 + 1"`, `calc "2 * 3 + 4"` or `quit`.
     // Operators: +, -, *, /, **, (), [], {}.
     // Should have prefix calculation like `calc "-5 + 4"`
     // Should handle error like zero-divisions.
-    // TODO 0: Create CLI environment(repl), takes commands; ['calc', 'end'].
+    // Create CLI environment(repl), takes commands; ['calc', 'end'].
     fmt.Println("Start calculator.")
     fmt.Println(">>>>")
     scanner := bufio.NewScanner(os.Stdin)
     for {
-        fmt.Print(">> ")
+        fmt.Print(REPL)
         scanner.Scan()
         cmd := scanner.Text()
 
@@ -28,11 +35,13 @@ func main() {
         case QUIT:
             return
         default:
-
-            fmt.Printf(">> result: %s\n", cmd)
+            calculatedResult, err := p.Evaluate(cmd)
+            if err != nil {
+                fmt.Printf("%sIncorrect prompt: %s\n", REPL, cmd)
+                continue
+            }
+            // Round the result to 2 decimal places
+            fmt.Printf(">> result: %.2f\n", calculatedResult)
         }
     }
-
-    // TODO 1: Read expressions +, -, *, /.
-    // Handle four simple expressions.
 }
